cecfc9b13d2ed897370d1b494412b17876c08f17
Tom Frampton <thomas.a.frampton@gmail.com>
2021-03-31 14:25:02 +0100

    Added functionality to move selected block using arrow keys

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,22 +1,69 @@
 import { FC, Children, useCallback, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AnyAction, Dispatch } from 'redux';
 
+import useMousetrap from 'react-hook-mousetrap';
 import styled, { css } from 'styled-components';
 
-import { createGrid } from 'reducers';
-import { INDEX } from 'typings/numbers';
+import { createGrid, IReducerState, selectBlock } from 'reducers';
+import { BLOCK_COORDS, INDEX } from 'typings/numbers';
 
 import { Block } from './block';
 
+interface IState {
+    selectedBlock?: BLOCK_COORDS
+}
+
 export const Grid: FC = () => {
     const dispatch = useDispatch<Dispatch<AnyAction>>();
+    const state = useSelector<IReducerState, IState>(({ selectedBlock }) => ({ selectedBlock }));
 
     const create = useCallback(() => dispatch(createGrid()), [dispatch]);
 
     useEffect(() => {
         create();
     }, [create]);
+
+    function moveUp() {
+        if (state.selectedBlock && state.selectedBlock.rowIndex > 0) {
+            dispatch(selectBlock({
+                rowIndex: state.selectedBlock.rowIndex - 1 as INDEX,
+                colIndex: state.selectedBlock.colIndex
+            }));
+        }
+    }
+
+    function moveDown() {
+        if (state.selectedBlock && state.selectedBlock.rowIndex < 8) {
+            dispatch(selectBlock({
+                rowIndex: state.selectedBlock.rowIndex + 1 as INDEX,
+                colIndex: state.selectedBlock.colIndex
+            }));
+        }
+    }
+
+    function moveLeft() {
+        if (state.selectedBlock && state.selectedBlock.colIndex > 0) {
+            dispatch(selectBlock({
+                rowIndex: state.selectedBlock.rowIndex,
+                colIndex: state.selectedBlock.colIndex - 1 as INDEX
+            }));
+        }
+    }
+
+    function moveRight() {
+        if (state.selectedBlock && state.selectedBlock.colIndex < 8) {
+            dispatch(selectBlock({
+                rowIndex: state.selectedBlock.rowIndex,
+                colIndex: state.selectedBlock.colIndex + 1 as INDEX
+            }));
+        }
+    }
+
+    useMousetrap('up', moveUp);
+    useMousetrap('down', moveDown);
+    useMousetrap('left', moveLeft);
+    useMousetrap('right', moveRight);
 
     return (
         <Container data-cy="grid-container">
